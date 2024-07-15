880306ab972c644cddbbfdf92c367d9915f218ad
Mike Berrio <mikeberrio.a@gmail.com>
2024-07-15 12:27:27 -0400

    fixed Delete request

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-const express = require('express');
-const mongoose = require ('mongoose');
-const multer = require('multer')
-const path = require ('path')
-const dotenv = require ('dotenv')
-const authRoute = require ('./routes/auth') //here im importing the auth.js file to start accessing the data from that file
-const usersRoute = require('./routes/users');
-const postsRoute = require('./routes/posts');
-const categoryRoute = require ('./routes/categories');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth"); //here im importing the auth.js file to start accessing the data from that file
+const usersRoute = require("./routes/users");
+const postsRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
 
-dotenv.config()
-const LOCAL =  process.env.SERVER_LOCAL
-const MONGO = process.env.MONGO_URL
-const CLIENT = process.env.REACT_APP_CLIENT
+dotenv.config();
+const LOCAL = process.env.SERVER_LOCAL;
+const MONGO = process.env.MONGO_URL;
+const CLIENT = process.env.REACT_APP_CLIENT;
 
 const app = express();
 
 app.use(express.json());
 // here im using the the path package that i installed to make the images folder in this back end to it public so that way the images uploaded will display in the Front End
-app.use('/images', express.static(path.join(__dirname,'/images')))
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 // here im connecting my local DB with Mongoose, i also can connect my MongoDB Atlas thru this line of code but for testing im doing it
 // in the local machine im also cons.log for any errors
-mongoose.connect(MONGO, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
-}).then(console.log("Connected to MongoDB")).catch((err) => console.log(err));
+mongoose
+  .connect(MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // here we are going to use multer to storage our images and then we are going to indicate the folder we are going to use
 const storage = multer.diskStorage({
@@ -37,26 +45,36 @@ const storage = multer.diskStorage({
   // and here will be the name we are providing
   filename: (req, file, cb) => {
     cb(null, req.body.name);
-  }
-})
-
-const upload = multer({storage:storage})
-app.post('/server/upload', upload.single("file"), (req, res) => {
-  res.status(200).json('Files has been uploaded')
-})  
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", CLIENT); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  },
 });
-// in here we use the app.use to start sending data to our DB, and i can use the Postman to test using localhost:5000/server/auth/register
-// creating a route for ex /server/auth and then we step in to the authRoute to grab the /register from the auth.js file 
-app.use('/server/auth', authRoute)
-app.use('/server/users', usersRoute)
-app.use('/server/posts', postsRoute)
-app.use('/server/categories', categoryRoute)
 
-app.listen (process.env.PORT || LOCAL, () => {  
-    console.log("backend is running");
-})
+const upload = multer({ storage: storage });
+app.post("/server/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("Files has been uploaded");
+});
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", CLIENT); // update to match the domain you will make the request from
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+app.use(
+  cors({
+    origin: CLIENT,
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
+// in here we use the app.use to start sending data to our DB, and i can use the Postman to test using localhost:5000/server/auth/register
+// creating a route for ex /server/auth and then we step in to the authRoute to grab the /register from the auth.js file
+app.use("/server/auth", authRoute);
+app.use("/server/users", usersRoute);
+app.use("/server/posts", postsRoute);
+app.use("/server/categories", categoryRoute);
+
+app.listen(process.env.PORT || LOCAL, () => {
+  console.log("backend is running");
+});
--- routes/posts.js
+++ routes/posts.js
@@ -1,102 +1,106 @@
-const router = require('express').Router(   );
+const router = require("express").Router();
 const Post = require("../models/Post");
 // const User = require("../models/User");
 
 /* Create Post */
-router.post('/', async (req, res) => {
-    // here im creating the newPost variable from the Post schema model, 
-    // where is going to receive from the body what the user is submitting
-    const newPost = new Post(req.body);
-    try {
-        // here im saving the newPost in to the DB
-        const savedPost = await newPost.save();
-        res.status(200).json(savedPost);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-})
+router.post("/", async (req, res) => {
+  // here im creating the newPost variable from the Post schema model,
+  // where is going to receive from the body what the user is submitting
+  const newPost = new Post(req.body);
+  try {
+    // here im saving the newPost in to the DB
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 /* Update Post */
 
-router.put ('/:id', async (req, res) => {
-    try {
-        // here i create the constant that is going to hold the post that im updating
-        const post = await Post.findById(req.params.id);
+router.put("/:id", async (req, res) => {
+  try {
+    // here i create the constant that is going to hold the post that im updating
+    const post = await Post.findById(req.params.id);
 
-        // then here im going to set a condition that is going to make sure this post belongs to the user that is updating
-        if (post.username === req.body.username) {
-            try {
-                // and here once the condition is true then is going to use the method to find and update the post
-                const updatedPost = await Post.findByIdAndUpdate (req.params.id, {$set:req.body},{new:true});
-                res.status(200).json(updatedPost);
-            } catch (error) {
-                res.status(500).json(error);
-            }
-        } else {
-            res.status(401).json('You can only update your Posts!')
-        }
-    } catch (error) {
-        res.status(500).json(error);    
+    // then here im going to set a condition that is going to make sure this post belongs to the user that is updating
+    if (post.username === req.body.username) {
+      try {
+        // and here once the condition is true then is going to use the method to find and update the post
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          { $set: req.body },
+          { new: true }
+        );
+        res.status(200).json(updatedPost);
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    } else {
+      res.status(401).json("You can only update your Posts!");
     }
-})
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 /* Delete Post */
 
-router.delete ('/:id', async (req, res) => {
-    try {
-        // here i create the constant that is going to hold the post that im deleting
-        const post = await Post.findById(req.params.id);
+router.delete("/:id", async (req, res) => {
+  try {
+    // here i create the constant that is going to hold the post that im deleting
+    const post = await Post.findById(req.params.id);
 
-        // then here im going to set a condition that is going to make sure this post belongs to the user that is deleting
-        if (post.username === req.body.username) {
-            try {
-                // and here once the condition is true then i can go straight to delete because in line 48 we already found the post
-                await post.delete();
-                res.status(200).json("Post is deleted!");
-            } catch (error) {
-                res.status(500).json(error);
-            }
-        } else {
-            res.status(401).json('You can only delete your Posts!')
-        }
-    } catch (error) {
-        res.status(500).json(error);    
+    // then here im going to set a condition that is going to make sure this post belongs to the user that is deleting
+    if (post.username === req.body.username) {
+      try {
+        // and here once the condition is true then i can go straight to delete because in line 48 we already found the post
+        await Post.findByIdAndDelete(req.params.id);
+        res.status(200).json("Post is deleted!");
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    } else {
+      res.status(401).json("You can only delete your Posts!");
     }
-})
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 /* Get Post */
 
-router.get('/:Id', async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.Id);
-        res.status(200).json(post);
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+router.get("/:Id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.Id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 /* Get All Posts  */
 
-router.get('/', async (req, res) => {
-    const username = req.query.user;
-    const catName = req.query.cat;
-    try {
-        let posts;
-        if (username) {
-            posts =await Post.find({username});
-        } else if (catName) {
-            posts = await Post.find ({
-                categories: {
-                    $in:[catName]
-                }
-            })
-        } else {
-            posts = await Post.find()
-        }
-        res.status(200).json(posts);
-    } catch (error) {
-        res.status(500).json(error)
+router.get("/", async (req, res) => {
+  const username = req.query.user;
+  const catName = req.query.cat;
+  try {
+    let posts;
+    if (username) {
+      posts = await Post.find({ username });
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName],
+        },
+      });
+    } else {
+      posts = await Post.find();
     }
-})
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
